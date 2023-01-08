3a20cb7aa1720ffef1656042567018043636cca8
Abu <abughalib64@gmail.com>
2023-01-08 19:48:54 +0530

    actions

---
+++ lib/appbar_actions.dart
@@ -0,0 +1,25 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class AppBarActions extends StatelessWidget {
+  AppBarActions({super.key, this.filePickerResult});
+
+  FilePickerResult? filePickerResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          onPressed: () async => {
+            filePickerResult = await FilePicker.platform.pickFiles(
+              type: FileType.custom,
+              allowedExtensions: ['pdf']
+            )
+          },
+          icon: const Icon(Icons.file_open),
+        )
+      ],
+    );
+  }
+}
