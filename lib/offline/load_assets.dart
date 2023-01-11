312a5500870fd07627d3616c43675bd3ec5c41fb
Abu Ghalib <abughalib64@gmailcom>
2023-01-11 19:41:08 +0530

    copy assets to application directory

---
+++ lib/offline/load_assets.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> openDB() async {
+  var databasesPath = await getDatabasesPath();
+  var path = join(databasesPath, "demo_asset_example.db");
+
+// Check if the database exists
+  var exists = await databaseExists(path);
+
+  if (!exists) {
+    // Should happen only the first time you launch your application
+    print("Creating new copy from asset");
+
+    // Make sure the parent directory exists
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (_) {}
+
+    // Copy from asset
+    ByteData data = await rootBundle.load(join("assets", "dictionary.db"));
+    List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    // Write and flush the bytes written
+    await File(path).writeAsBytes(bytes, flush: true);
+  } else {
+    print("Opening existing database");
+  }
+
+// open the database
+  var db = await openDatabase(path, readOnly: true);
+  return db;
+}
