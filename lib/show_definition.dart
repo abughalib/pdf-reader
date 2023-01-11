a8fd9d082335e4c95b69bf403f885a5a52fed229
Abu <abughalib64@gmail.com>
2023-01-12 03:11:04 +0530

    added online fetching as well

--- lib/show_definition.dart
+++ lib/show_definition.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pdf_reader/offline/db_op.dart';
 import 'package:pdf_reader/offline/models.dart';
+import 'package:pdf_reader/online/fetch_definition.dart';
+import 'package:pdf_reader/online/models.dart';
+
+// To be moved to utils
+enum Connectivity {
+  online,
+  offline,
+}
 
 class ShowDefinition extends StatefulWidget {
   const ShowDefinition({super.key, required this.selectedWord});
@@ -12,10 +20,29 @@ class ShowDefinition extends StatefulWidget {
 }
 
 class _ShowDefinitionState extends State<ShowDefinition> {
-  String meaning = "";
 
-  Future<String> fetchDefinition() async {
-    DefinitionQueryResult result = await queryMeaning(widget.selectedWord);
+  // Check Internet Connectivity here;
+  Connectivity connectivity = Connectivity.online;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (connectivity) {
+      case Connectivity.online:
+        return OnlineMeaning(selectedWord: widget.selectedWord);
+      case Connectivity.offline:
+        return OfflineMeaning(selectedWord: widget.selectedWord);
+    }
+  }
+}
+
+class OfflineMeaning extends StatelessWidget {
+  const OfflineMeaning({super.key, required this.selectedWord});
+
+  final String selectedWord;
+  final String meaning = "";
+
+  Future<String> queryDefinition() async {
+    DefinitionQueryResult result = await queryMeaning(selectedWord);
 
     return result.isOk() ? result.definition.meaning : result.getError();
   }
@@ -26,13 +53,113 @@ class _ShowDefinitionState extends State<ShowDefinition> {
       width: 20,
       alignment: Alignment.center,
       child: FutureBuilder(
-        future: fetchDefinition(),
+        future: queryDefinition(),
         initialData: "Fetching...",
         builder: (context, AsyncSnapshot<String> snapshot) =>
             SingleChildScrollView(
           child: Text(snapshot.data ?? ""),
         ),
       ),
+    );
+  }
+}
+
+class OnlineMeaning extends StatelessWidget {
+  OnlineMeaning({super.key, required this.selectedWord});
+
+  final String selectedWord;
+  final Meaning meaning = Meaning(def: [], keywords: []);
+
+  Future<DefinitionFormat> fetchDefinition() async {
+    OnlineDefinitionResult result = await definitionResult(selectedWord);
+    return DefinitionFormat(onlineDefinitionResult: result);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: FutureBuilder<Widget>(
+        future: fetchDefinition(),
+        initialData: DefinitionFormat(
+          onlineDefinitionResult:
+              OnlineDefinitionResult.status(FetchStatus.loading),
+        ),
+        builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) =>
+            snapshot.data!,
+      ),
+    );
+  }
+}
+
+class DefinitionFormat extends StatelessWidget {
+  const DefinitionFormat({Key? key, required this.onlineDefinitionResult})
+      : super(key: key);
+
+  final OnlineDefinitionResult onlineDefinitionResult;
+
+  @override
+  Widget build(BuildContext context) {
+    if (onlineDefinitionResult.fetchStatus == FetchStatus.loading) {
+      return const Text("Loading Meaning...");
+    } else if (onlineDefinitionResult.fetchStatus == FetchStatus.error) {
+      return const Text("Error...");
+    } else {
+      return SizedBox(
+        height: 300,
+        width: 200,
+        // BoxConstraints are useless for Alert Dialog
+        // constraints: const BoxConstraints(
+        //   maxHeight: double.maxFinite,
+        //   maxWidth: double.maxFinite,
+        // ),
+        child: FormatMeaning(
+          meaning: onlineDefinitionResult.onlineDefinition.meaning,
+        ),
+      );
+    }
+  }
+}
+
+class FormatMeaning extends StatelessWidget {
+  const FormatMeaning({Key? key, required this.meaning}) : super(key: key);
+
+  final Meaning meaning;
+
+  TextStyle getFontStyle(int index) {
+    if (index.isEven) {
+      return const TextStyle(fontWeight: FontWeight.bold);
+    }
+    return const TextStyle();
+  }
+
+  SizedBox listItemSpace(int index) {
+    if (index.isEven) {
+      return const SizedBox();
+    }
+    return const SizedBox(height: 15);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Text(
+    //   "${meaning.def[0]}\n${meaning.def[1]}"
+    // );
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: meaning.def.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          // Move value to constants
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: Text(
+            meaning.def[index],
+            style: getFontStyle(index),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return listItemSpace(index);
+      },
     );
   }
 }
