307f6476fbadd3a9404339bdbca70571407ae0d5
Abu <abughalib64@gmail.com>
2023-01-13 15:31:01 +0530

    PDF file preview

--- lib/preview.dart
+++ lib/preview.dart
@@ -1,16 +1,35 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pdf_reader/providers/file_pick_provider.dart';
 import 'package:pdf_reader/show_definition.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class PDFPreview extends StatefulWidget {
+class PDFPreview extends StatelessWidget {
   const PDFPreview({Key? key}) : super(key: key);
 
   @override
-  State<PDFPreview> createState() => _PDFPreviewState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: const OfflinePDFViewer(),
+      ),
+    );
+  }
 }
 
-class _PDFPreviewState extends State<PDFPreview> {
+class OfflinePDFViewer extends StatefulWidget {
+  const OfflinePDFViewer({Key? key}) : super(key: key);
+
+  @override
+  State<OfflinePDFViewer> createState() => _OfflinePDFViewerState();
+}
+
+class _OfflinePDFViewerState extends State<OfflinePDFViewer> {
   late PdfViewerController _pdfViewerController;
   String selectedText = "";
 
@@ -22,18 +41,28 @@ class _PDFPreviewState extends State<PDFPreview> {
 
   OverlayEntry? _overlayEntry;
 
-  Future<String?> _showDefinition() async {
+  _showDefinition() async {
     _pdfViewerController.clearSelection();
-    return showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
-        titlePadding: const EdgeInsets.all(10.0),
-        title: Text(selectedText),
-        scrollable: true,
-        content: ShowDefinition(selectedWord: selectedText),
-      ),
-    );
+    if (selectedText.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: SizedBox(
+            child: Text("Nothing selected"),
+          ),
+        ),
+      );
+    } else {
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+          titlePadding: const EdgeInsets.all(10.0),
+          title: Text(selectedText),
+          scrollable: true,
+          content: ShowDefinition(selectedWord: selectedText),
+        ),
+      );
+    }
   }
 
   void _showContextMenu(
@@ -76,27 +105,50 @@ class _PDFPreviewState extends State<PDFPreview> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: SfPdfViewer.network(
-          'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
-          onTextSelectionChanged: (PdfTextSelectionChangedDetails details) {
-            if (details.selectedText == null && _overlayEntry != null) {
-              _overlayEntry!.remove();
-              _overlayEntry = null;
-            } else if (details.selectedText != null && _overlayEntry == null) {
-              _showContextMenu(context, details);
-              String selectedTexts = details.selectedText ?? "";
-              String firstWord = selectedTexts.split(" ").first.trim();
-              final String selectedTextTrimmed =
-                  firstWord.replaceAll(RegExp('[^a-zA-Z]'), '');
-              selectedText = selectedTextTrimmed;
-            }
-          },
-          controller: _pdfViewerController,
-        ),
-      ),
-    );
+    // TODO for web
+    // Byte Streaming
+    // Uint8List uploadFile = result.files.single.bytes;
+    //
+    // String filename = basename(result.files.single.name);
+    //
+    // fs.Reference storageRef = fs.FirebaseStorage.instance.ref().child('$dirpath$filename');
+    //
+    // final fs.UploadTask uploadTask = storageRef.putData(uploadFile);
+    //
+    // final fs.TaskSnapshot downloadUrl = await uploadTask;
+    //
+    // final String attachUrl = (await downloadUrl.ref.getDownloadURL());
+
+    FilePickerResult? filePickerResult =
+        context.watch<PDFFilePath>().filePickerResult;
+
+    if (filePickerResult == null || filePickerResult.paths.isEmpty) {
+      // TODO show better message no file selection
+      return const SizedBox(
+        child: Text("No File Selected"),
+      );
+    } else {
+      PlatformFile pdf =
+          context.watch<PDFFilePath>().filePickerResult!.files.single;
+      return SfPdfViewer.file(
+        // TODO better error handle if the file is corrupted
+        // Single PDF File Selection
+        File(pdf.path!),
+        onTextSelectionChanged: (PdfTextSelectionChangedDetails details) {
+          if (details.selectedText == null && _overlayEntry != null) {
+            _overlayEntry!.remove();
+            _overlayEntry = null;
+          } else if (details.selectedText != null && _overlayEntry == null) {
+            _showContextMenu(context, details);
+            String selectedTexts = details.selectedText ?? "";
+            String firstWord = selectedTexts.trim().split(" ").first;
+            final String selectedTextTrimmed =
+                firstWord.replaceAll(RegExp(r'[^a-zA-Z]'), '');
+            selectedText = selectedTextTrimmed;
+          }
+        },
+        controller: _pdfViewerController,
+      );
+    }
   }
 }
