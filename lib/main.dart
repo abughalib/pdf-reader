1c7cdb183b8974a9eae57f7072da10012f049749
Abu <abughalib64@gmail.com>
2023-01-13 15:30:35 +0530

    file picker provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:pdf_reader/appbar_actions.dart';
+import 'package:pdf_reader/providers/file_pick_provider.dart';
 import 'package:pdf_reader/recent_page.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
-  runApp(App());
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => PDFFilePath())],
+    child: const App(),
+  ));
 }
 
 class App extends StatelessWidget {
-  App({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +20,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Recent'),
-          actions: [AppBarActions()],
+          actions: const [AppBarActions()],
           backgroundColor: Colors.black87,
         ),
         body: const RecentPage(),
